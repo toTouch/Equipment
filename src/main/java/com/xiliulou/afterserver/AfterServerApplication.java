c04a2d26b19ad287119f260336263930647814cb
Eclair <1269029723@qq.com>
2021-01-13 09:37:13 +0800

    init

---
+++ src/main/java/com/xiliulou/afterserver/AfterServerApplication.java
@@ -0,0 +1,13 @@
+package com.xiliulou.afterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterServerApplication.class, args);
+	}
+
+}
