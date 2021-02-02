d587c6ebe69f8b1ad4e3b2738c721f8bdfafeabc
yg <1030245747@qq.com>
2021-02-02 19:28:09 +0800

    日志

--- src/main/java/com/xiliulou/afterserver/util/password/PasswordUtils.java
+++ src/main/java/com/xiliulou/afterserver/util/password/PasswordUtils.java
@@ -18,29 +18,29 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class PasswordUtils {
-	private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-	/**
-	 * 加密
-	 *
-	 * @param str 字符串
-	 * @return 返回加密字符串
-	 */
-	public static String encode(String str) {
-		return passwordEncoder.encode(str);
-	}
+    /**
+     * 加密
+     *
+     * @param str 字符串
+     * @return 返回加密字符串
+     */
+    public static String encode(String str) {
+        return passwordEncoder.encode(str);
+    }
 
-	/**
-	 * 比较密码是否相等
-	 *
-	 * @param str      明文密码
-	 * @param password 加密后密码
-	 * @return true：成功    false：失败
-	 */
-	public static boolean matches(String str, String password) {
-		log.info("str:[{}],password:[{}]", str, password);
-		return passwordEncoder.matches(str, password);
-	}
+    /**
+     * 比较密码是否相等
+     *
+     * @param str      明文密码
+     * @param password 加密后密码
+     * @return true：成功    false：失败
+     */
+    public static boolean matches(String str, String password) {
+
+        return passwordEncoder.matches(str, password);
+    }
 
 
 	/*public static void main(String[] args) {
