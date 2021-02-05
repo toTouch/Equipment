f13bae2b48dddb63ca90fc44121c90389facbf39
yg <1030245747@qq.com>
2021-02-05 09:52:39 +0800

    对账

--- src/main/java/com/xiliulou/afterserver/web/vo/WorkOrderVo.java
+++ src/main/java/com/xiliulou/afterserver/web/vo/WorkOrderVo.java
@@ -4,6 +4,7 @@ import com.xiliulou.afterserver.entity.File;
 import com.xiliulou.afterserver.entity.WorkOrder;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -13,8 +14,27 @@ import java.util.List;
  * @create: 2021-02-01 10:00
  **/
 @Data
-public class WorkOrderVo extends WorkOrder {
-
+public class WorkOrderVo  {
+    private Long id;
+    private Integer type;
+    private Long pointId;
+    private String info;
+    private BigDecimal fee;
+    private Long thirdCompanyId;
+    private Integer thirdCompanyType;
+    private BigDecimal thirdCompanyPay;
+    private Long workOrderReasonId;
+    //服务商id
+    private Long serverId;
+    //服务商接单人
+    private String processor;
+    private Long processTime;
+    private Integer status;
+    private String orderNo;
+    private Long createrId;
+    private Long createTime;
+    private String thirdCompanyName;
+    private String serverName;
     private String workOrderReasonName;
     private String workOrderType;
     private String creater;
