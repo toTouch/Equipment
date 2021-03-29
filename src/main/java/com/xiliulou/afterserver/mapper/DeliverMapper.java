2b61f1cccda0adc38cb5e808e9e6027b1ddc4cee
“wwh” <you>
2021-03-29 13:06:14 +0800

    仓库列表

--- src/main/java/com/xiliulou/afterserver/mapper/DeliverMapper.java
+++ src/main/java/com/xiliulou/afterserver/mapper/DeliverMapper.java
@@ -1,15 +1,10 @@
 package com.xiliulou.afterserver.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xiliulou.afterserver.entity.Deliver;
-import com.xiliulou.afterserver.web.query.DeliverQuery;
-import com.xiliulou.afterserver.web.vo.DeliverVo;
-import org.apache.ibatis.annotations.Param;
 
 public interface DeliverMapper extends BaseMapper<Deliver> {
 
-    IPage<DeliverVo> getDeliverPage(Page page, @Param("query") DeliverQuery deliver);
+//    IPage<DeliverVo> getDeliverPage(Page page, @Param("query") DeliverQuery deliver);
 
 }
