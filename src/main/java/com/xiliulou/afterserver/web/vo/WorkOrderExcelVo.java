4f133e5a48c496f0078a04b08069c219bd7afc5c
“wwh” <you>
2021-04-06 11:03:00 +0800

    工单导出字段调整

--- src/main/java/com/xiliulou/afterserver/web/vo/WorkOrderExcelVo.java
+++ src/main/java/com/xiliulou/afterserver/web/vo/WorkOrderExcelVo.java
@@ -16,29 +16,27 @@ public class WorkOrderExcelVo {
 
     @ExcelProperty("工单类型")
     private String workOrderType;
-    @ExcelProperty("第三方公司名称")
+    @ExcelProperty("点位")
     private String thirdCompanyName;
     @ExcelProperty("费用")
     private BigDecimal fee;
+    @ExcelProperty("公司")
+    private String thirdCompanyId;
     @ExcelProperty("第三方承担费用")
     private BigDecimal thirdCompanyPay;
-    @ExcelProperty("服务商名称")
-    private String serverName;
+    @ExcelProperty("工单原因")
+    private String workOrderReasonName;
     @ExcelProperty("处理人")
     private String processor;
+    @ExcelProperty("服务商名称")
+    private String serverName;
+    @ExcelProperty("创建人")
+    private String creater;
     @ExcelProperty("处理时间")
     private String processorTimeStr;
     @ExcelProperty("工单状态")
     private String statusStr;
     @ExcelProperty("工单编号")
     private String orderNo;
-    @ExcelProperty("创建人")
-    private String creater;
-    @ExcelProperty("工单原因")
-    private String workOrderReasonName;
-    @ExcelProperty("描述")
-    private String info;
-    @ExcelProperty("创建时间")
-    private String createTimeStr;
 
 }
