804c25b4ac767a491a260b0c8c46a6e812971749
Hardy <1648260462@qq.com>
2021-04-25 18:08:31 +0800

    加入城市联动

---
+++ src/main/java/com/xiliulou/afterserver/service/CityService.java
@@ -0,0 +1,18 @@
+package com.xiliulou.afterserver.service;
+
+
+
+
+import com.xiliulou.afterserver.util.R;
+
+/**
+ * (City)表服务接口
+ *
+ * @author makejava
+ * @since 2021-01-21 18:05:41
+ */
+public interface CityService {
+
+
+    R cityTree();
+}
