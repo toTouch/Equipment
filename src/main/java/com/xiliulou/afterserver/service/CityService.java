9730f58c8e59339b3f61ac39f2c8bb22590f5e49
Hardy <1648260462@qq.com>
2021-04-26 10:19:08 +0800

    加入供应商城市转换cityID

--- src/main/java/com/xiliulou/afterserver/service/CityService.java
+++ src/main/java/com/xiliulou/afterserver/service/CityService.java
@@ -3,6 +3,8 @@ package com.xiliulou.afterserver.service;
 
 
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xiliulou.afterserver.entity.SysAreaCodeEntity;
 import com.xiliulou.afterserver.util.R;
 
 /**
@@ -11,7 +13,7 @@ import com.xiliulou.afterserver.util.R;
  * @author makejava
  * @since 2021-01-21 18:05:41
  */
-public interface CityService {
+public interface CityService extends IService<SysAreaCodeEntity> {
 
 
     R cityTree();
