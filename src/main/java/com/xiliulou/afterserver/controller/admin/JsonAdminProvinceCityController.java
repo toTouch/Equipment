15c058036d98164db01b9f05339844cff9f723f5
Hardy <1648260462@qq.com>
2021-06-10 17:53:12 +0800

    加入城市

---
+++ src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminProvinceCityController.java
@@ -0,0 +1,34 @@
+package com.xiliulou.afterserver.controller.admin;
+
+import com.xiliulou.afterserver.service.CityService;
+import com.xiliulou.afterserver.service.ProvinceService;
+
+import com.xiliulou.afterserver.util.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: eclair
+ * @Date: 2021/1/21 18:56
+ * @Description:
+ */
+@RestController
+public class JsonAdminProvinceCityController {
+	@Autowired
+	CityService cityService;
+	@Autowired
+	ProvinceService provinceService;
+
+	@GetMapping(value="/admin/city/{pid}")
+	public R getCityList(@PathVariable("pid")Integer pid) {
+		return R.ok(cityService.queryCityListByPid(pid));
+	}
+
+	@GetMapping(value="/admin/province/list")
+	public R getProvinceList() {
+		return R.ok(provinceService.queryList());
+	}
+
+}
---
+++ src/main/java/com/xiliulou/afterserver/service/CityService.java
@@ -0,0 +1,71 @@
+package com.xiliulou.afterserver.service;
+
+
+
+
+import com.xiliulou.afterserver.entity.City;
+
+import java.util.List;
+
+/**
+ * (City)表服务接口
+ *
+ * @author makejava
+ * @since 2021-01-21 18:05:41
+ */
+public interface CityService {
+
+    /**
+     * 通过ID查询单条数据从数据库
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    City queryByIdFromDB(Integer id);
+    
+      /**
+     * 通过ID查询单条数据从缓存
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    City queryByIdFromCache(Integer id);
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<City> queryAllByLimit(int offset, int limit);
+
+    /**
+     * 新增数据
+     *
+     * @param city 实例对象
+     * @return 实例对象
+     */
+    City insert(City city);
+
+    /**
+     * 修改数据
+     *
+     * @param city 实例对象
+     * @return 实例对象
+     */
+    Integer update(City city);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    Boolean deleteById(Integer id);
+
+    City queryCityByCode(String cityCode);
+
+    List<City> queryCityListByPid(Integer pid);
+
+}
