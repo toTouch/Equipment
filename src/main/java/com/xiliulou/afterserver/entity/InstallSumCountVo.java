090f24dc3903a41a3ed6b0610eedb05ec70f2f7f
Hardy <1648260462@qq.com>
2021-06-17 19:24:16 +0800

    修改安装统计

---
+++ src/main/java/com/xiliulou/afterserver/entity/InstallSumCountVo.java
@@ -0,0 +1,18 @@
+package com.xiliulou.afterserver.entity;
+
+import com.xiliulou.afterserver.web.vo.AfterOrderVo;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Hardy
+ * @date 2021/6/17 0017 19:20
+ * @Description:
+ */
+@Data
+public class InstallSumCountVo {
+    private Long pointId;
+    private Double sum;
+    private List<AfterOrderVo> list;
+}
