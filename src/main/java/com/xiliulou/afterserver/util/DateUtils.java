66cf8c5df094b67b42e3f5be0a33c84920cb7899
15877514420 <1648260462@qq.com>
2021-06-29 13:48:09 +0800

    统计

--- src/main/java/com/xiliulou/afterserver/util/DateUtils.java
+++ src/main/java/com/xiliulou/afterserver/util/DateUtils.java
@@ -84,7 +84,7 @@ public class DateUtils {
      */
     public static String stampToDate(String s){
         String res;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         long lt = new Long(s);
         Date date = new Date(lt);
         res = simpleDateFormat.format(date);
