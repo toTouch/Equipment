9c8c454855f1ee8453bd8d8e6d4f5289fed3547f
Hardy <1648260462@qq.com>
2021-08-19 15:48:59 +0800

    修改序列号生成规则

--- src/main/java/com/xiliulou/afterserver/util/DataUtil.java
+++ src/main/java/com/xiliulou/afterserver/util/DataUtil.java
@@ -11,10 +11,11 @@ public class DataUtil {
     }
 
     public static String getDateFormat(Long dates){
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        SimpleDateFormat sdf=new SimpleDateFormat("yy-MM-dd HH:mm:ss");
         String sd = sdf.format(new Date(Long.parseLong(String.valueOf(dates))));
         return sd;
     }
+
 
     public static void main(String[] args) {
 
@@ -23,8 +24,11 @@ public class DataUtil {
 
     }
 
-    public static Long getNo(){
-        return Long.parseLong(getDateFormat(System.currentTimeMillis()));
+    public static String getNo(){
+        String dateFormat = getDateFormat(System.currentTimeMillis());
+        String[] split = dateFormat.split("-");
+        String resout = "xll" + split[0] + split[1] + (int) ((Math.random() * 9 + 1) * 100000);
+        return  resout;
     }
 
 
