cffb6389cbd7f7b86f2deb0e5b36be3ba798d082
zgw <1311510078@qq.com>
2021-09-27 09:28:27 +0800

    修改 1.

--- src/main/java/com/xiliulou/afterserver/service/impl/DeliverServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/DeliverServiceImpl.java
@@ -105,6 +105,10 @@ public class DeliverServiceImpl extends ServiceImpl<DeliverMapper, Deliver> impl
                records.setUserName(userById.getUserName());
             }
 
+            if(records.getState() == 1){
+                records.setDeliver(true);
+            }
+
 //            第三方类型 1：客户 2：供应商 3:服务商';
             if (Objects.nonNull(records.getThirdCompanyType())){
                 String name = "";
@@ -148,7 +152,6 @@ public class DeliverServiceImpl extends ServiceImpl<DeliverMapper, Deliver> impl
                     }
                 }
                 records.setDetails(map);
-                records.setDeliver(true);
             }
         });
 
