c57025ac397861b71ef5be70ee16da1197cd1226
zgw <1311510078@qq.com>
2021-09-30 19:50:56 +0800

    修改 1.

--- src/main/java/com/xiliulou/afterserver/service/impl/DeliverServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/DeliverServiceImpl.java
@@ -201,10 +201,10 @@ public class DeliverServiceImpl extends ServiceImpl<DeliverMapper, Deliver> impl
             }
             //productAndNum
             if(!StrUtil.isEmpty(d.getProduct())
-                    && d.getProduct().matches("[.*]")
+                    && d.getProduct().matches("\\[.*\\]")
                     && !StrUtil.isEmpty(d.getQuantity())
                     && !d.getQuantity().equals("[null]")
-                    && d.getQuantity().matches("[.*]")){
+                    && d.getQuantity().matches("\\[.*\\]")){
 
                 ArrayList<Integer> products = JSON.parseObject(d.getProduct(), ArrayList.class);
                 ArrayList<Integer> quantitys = JSON.parseObject(d.getQuantity(), ArrayList.class);
