29ae673b173813a5b16a4037db52851705745743
叶书箖 <1311510078@qq.com>
2021-12-28 23:39:35 +0800

    解决跨域

--- src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminUserPermissionController.java
+++ src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminUserPermissionController.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin(origins = "*")
 public class JsonAdminUserPermissionController extends BaseController {
 
     @Autowired
