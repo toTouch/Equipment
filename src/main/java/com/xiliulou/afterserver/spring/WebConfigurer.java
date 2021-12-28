06a805e2e82306e81c8a89b104ac8f73e609c22d
叶书箖 <1311510078@qq.com>
2021-12-28 22:28:16 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/spring/WebConfigurer.java
+++ src/main/java/com/xiliulou/afterserver/spring/WebConfigurer.java
@@ -6,18 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfigurer implements WebMvcConfigurer {
-    @Override
+//@Configuration
+public class WebConfigurer {
+    //@Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 跨域拦截器需放在最上面
 //		registry.addInterceptor(getAuthInterceptorAdapter()).addPathPatterns("/**");
         //拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录
 
-        registry.addInterceptor(getAuthInterceptorAdapter()).addPathPatterns("/**");
+       // registry.addInterceptor(getAuthInterceptorAdapter()).addPathPatterns("/**");
     }
 
-    @Bean
+    //@Bean
     public AuthInterceptorAdapter getAuthInterceptorAdapter() {
         return new AuthInterceptorAdapter();
     }
