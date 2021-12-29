e8299100a2b2db9caa53d20b7759d1fc0d6eecce
叶书箖 <1311510078@qq.com>
2021-12-29 11:17:03 +0800

    bugfix

--- src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminProvinceCityController.java
+++ src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminProvinceCityController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
-@CrossOrigin("*")
 public class JsonAdminProvinceCityController {
 	@Autowired
 	CityService cityService;
