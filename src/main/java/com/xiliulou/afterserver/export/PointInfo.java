68a0b9b928f2d3ee21732b799461c14a764bd867
叶书箖 <1311510078@qq.com>
2022-01-24 17:06:00 +0800

    修改导入模板

--- src/main/java/com/xiliulou/afterserver/export/PointInfo.java
+++ src/main/java/com/xiliulou/afterserver/export/PointInfo.java
@@ -13,84 +13,81 @@ import java.math.BigDecimal;
 @Data
 public class PointInfo {
     /**
-     * 名称
+     * 产品系列
      */
     @ExcelProperty(index = 0)
-    private String name;
-    /**
-     * 客户
-     */
-    @ExcelProperty(index = 1)
-    private String customerId;
-
-    /**
-     * 点位状态
-     */
-    @ExcelProperty(index = 2)
-    private Integer status;
-
-    /**
-     * 摄像头数量
-     */
-    @ExcelProperty(index = 3)
-    private Integer cameraCount;
-
-    /**
-     * 雨棚数量
-     */
-    @ExcelProperty(index = 4)
-    private Integer canopyCount;
-
+    private String productSeries;
     /**
      * 城市
      */
-    @ExcelProperty(index = 5)
+    @ExcelProperty(index = 1)
     private String city;
+    /**
+     * 客户
+     */
+    @ExcelProperty(index = 2)
+    private String customerId;
+    /**
+     * 名称
+     */
+    @ExcelProperty(index = 3)
+    private String name;
+    /**
+     * 点位状态
+     */
+    @ExcelProperty(index = 4)
+    private Integer status;
     /**
      * 安装类型 1:室外 2:半室外3：室内
      */
-    @ExcelProperty(index = 6)
+    @ExcelProperty(index = 5)
     private String installType;
     /**
-     * 安装时间
+     * 雨棚数量
      */
-    @ExcelProperty(index = 7)
-    private String installTime;
-    /**
-     * '物联网卡供应商
-     */
-    @ExcelProperty(index = 8)
-    private String cardSupplier;
+    @ExcelProperty(index = 6)
+    private Integer canopyCount;
     /**
      *sn码
      */
-    @ExcelProperty(index = 9)
+    @ExcelProperty(index = 7)
     private String snNo;
     /**
      * 物联网卡
      */
-    @ExcelProperty(index = 10)
+    @ExcelProperty(index = 8)
     private String cardNumber;
+    /**
+     * '物联网卡供应商
+     */
+    @ExcelProperty(index = 9)
+    private String cardSupplier;
+
+    /**
+     * 摄像头数量
+     */
+    @ExcelProperty(index = 10)
+    private Integer cameraCount;
+    /**
+     * 安装时间
+     */
+    @ExcelProperty(index = 11)
+    private String installTime;
     /**
      * 施工完成时间
      */
-    @ExcelProperty(index = 11)
+    @ExcelProperty(index = 12)
     private String completionTime;
     /**
      * 入账
      */
-    @ExcelProperty(index = 12)
+    @ExcelProperty(index = 13)
     private String isEntry;
     /**
      * 验收
      */
-    @ExcelProperty(index = 13)
-    private String isAcceptance;
-    /**
-     * 订单来源
-     */
     @ExcelProperty(index = 14)
-    private String productSeries;
+    private String isAcceptance;
     /**
      * 下单时间
      */
