4f8354bad09783830131729040b4b7456aba9fe1
zgw <1311510078@qq.com>
2022-03-03 14:03:58 +0800

    添加手持终端首页获取工厂信息

---
+++ src/main/java/com/xiliulou/afterserver/web/query/FactoryUserInfoVo.java
@@ -0,0 +1,13 @@
+package com.xiliulou.afterserver.web.query;
+
+import lombok.Data;
+
+/**
+ * @author zgw
+ * @date 2022/3/3 13:56
+ * @mood
+ */
+@Data
+public class FactoryUserInfoVo {
+    private String supplierName;
+}
