1534c5668468f4e547163ad1c1a405e0bef04b28
zgw <1311510078@qq.com>
2022-03-24 10:55:36 +0800

    oss上传测试

--- src/main/java/com/xiliulou/afterserver/service/impl/FileServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/FileServiceImpl.java
@@ -120,9 +120,9 @@ public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements Fi
      */
     @Override
     public R uploadFileToOss(MultipartFile file) {
-        //String fileName = storageConfig.getDir().replaceAll("/","") + StrUtil.DASHED
-               // + IdUtil.simpleUUID() + StrUtil.DOT + FileUtil.extName(file.getOriginalFilename());
-        String fileName = "after-service/zgwTest++++++++gegege." + FileUtil.extName(file.getOriginalFilename());
+        String fileName = storageConfig.getDir().replaceAll("/","") + StrUtil.DASHED
+                + IdUtil.simpleUUID() + StrUtil.DOT + FileUtil.extName(file.getOriginalFilename());
+
         try {
             aliyunOssService.uploadFile("liubiqi",
                     fileName,
