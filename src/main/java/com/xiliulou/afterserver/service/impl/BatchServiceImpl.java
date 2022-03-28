d37d112c7fd1e434e2cb4c9d91a7eaecc6880d16
zgw <1311510078@qq.com>
2022-03-28 13:37:59 +0800

    修改用户表供应商id字段为第三方id

--- src/main/java/com/xiliulou/afterserver/service/impl/BatchServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/BatchServiceImpl.java
@@ -248,12 +248,12 @@ public class BatchServiceImpl implements BatchService {
             return R.fail("未查询到相关用户");
         }
 
-        Supplier supplier = supplierService.getById(user.getSupplierId());
+        Supplier supplier = supplierService.getById(user.getThirdId());
         if(Objects.isNull(supplier)){
             return R.fail("用户未绑定工厂，请联系管理员");
         }
         Page page = PageUtil.getPage(offset, size);
-        page = batchMapper.selectPage(page, new QueryWrapper<Batch>().eq("supplier_id", user.getSupplierId()));
+        page = batchMapper.selectPage(page, new QueryWrapper<Batch>().eq("supplier_id", user.getThirdId()));
 
         List<Batch> batchList = page.getRecords();
 
