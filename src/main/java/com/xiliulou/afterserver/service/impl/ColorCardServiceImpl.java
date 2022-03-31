966c7a5a30b10774753816840dc4122bd7f781cf
zgw <1311510078@qq.com>
2022-03-31 09:42:12 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/service/impl/ColorCardServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/ColorCardServiceImpl.java
@@ -1,11 +1,13 @@
 package com.xiliulou.afterserver.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiliulou.afterserver.entity.ColorCard;
 import com.xiliulou.afterserver.mapper.ColorCardMapper;
 import com.xiliulou.afterserver.service.ColorCardService;
 import com.xiliulou.afterserver.util.R;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,6 +20,6 @@ public class ColorCardServiceImpl extends ServiceImpl<ColorCardMapper, ColorCard
 
     @Override
     public R queryPull(String name){
-        return R.ok(this.baseMapper.selectList(new QueryWrapper<ColorCard>().like("name", name)));
+        return R.ok(this.baseMapper.selectList(new LambdaQueryWrapper<ColorCard>().like(StringUtils.isNotBlank(name), ColorCard::getName, name)));
     }
 }
