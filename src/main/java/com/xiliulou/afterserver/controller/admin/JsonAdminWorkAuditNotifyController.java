c22058dea1367ed62e8b74419b529c92166f01fe
zgw <1311510078@qq.com>
2022-04-02 19:39:30 +0800

    bug fix

---
+++ src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminWorkAuditNotifyController.java
@@ -0,0 +1,40 @@
+package com.xiliulou.afterserver.controller.admin;
+
+import com.xiliulou.afterserver.service.WorkAuditNotifyService;
+import com.xiliulou.afterserver.util.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author zgw
+ * @date 2022/4/2 18:23
+ * @mood
+ */
+@RestController
+public class JsonAdminWorkAuditNotifyController {
+
+    @Autowired
+    WorkAuditNotifyService workAuditNotifyService;
+
+    @GetMapping("/admin/WorkAuditNotify/list")
+    public R queryList(@RequestParam("size") Integer size,
+                       @RequestParam("offset") Integer offset,
+                       @RequestParam("type") Integer type){
+        return workAuditNotifyService.queryList(size, offset, type);
+    }
+
+    @GetMapping("/admin/WorkAuditNotify/count")
+    public R queryCount(@RequestParam("type") Integer type){
+        return workAuditNotifyService.queryCount(type);
+    }
+
+    @GetMapping("/admin/WorkAuditNotify/read/notify/{id}")
+    public R readNotify(@PathVariable("id") Long id){
+        return workAuditNotifyService.readNotify(id);
+    }
+
+    @GetMapping("/admin/WorkAuditNotify/read/notify/all")
+    public R readNotifyAll(){
+        return workAuditNotifyService.readNotifyAll();
+    }
+}
