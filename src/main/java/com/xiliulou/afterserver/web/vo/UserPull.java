61d503dbe879b0efc0098dcc8723c86865f03df3
zgw <1311510078@qq.com>
2022-04-07 10:52:11 +0800

    bug fix

---
+++ src/main/java/com/xiliulou/afterserver/web/vo/UserPull.java
@@ -0,0 +1,11 @@
+package com.xiliulou.afterserver.web.vo;
+
+/**
+ * @author zgw
+ * @date 2022/4/7 10:45
+ * @mood
+ */
+public class UserPull {
+    private Long id;
+    private String userName;
+}
