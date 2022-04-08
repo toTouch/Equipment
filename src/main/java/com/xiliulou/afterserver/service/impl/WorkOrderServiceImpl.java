652d3f0e7415c7cf1e4e2309029c220890e514b7
zgw <1311510078@qq.com>
2022-04-08 11:00:46 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
@@ -242,7 +242,7 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
         List<List<String>> headList = new ArrayList<List<String>>();
 
         String[] header = {"审核状态", "工单类型", "点位", "点位状态", "移机起点", "移机终点", "创建人",
-                "状态", "描述", "备注", "工单原因", "创建时间", "工单编号", "sn码", "审核内容"};
+                "状态", "描述", "备注", "工单原因", "创建时间", "工单编号", "sn码", "审核内容","专员", "派单时间"};
 
         String[] serverHeader = {"服务商", "费用", "结算方式", "解决方案", "解决时间", "处理时长", "文件个数", " 第三方类型", "第三方公司", "第三方费用", "支付状态", "第三方原因", "第三方对接人"};
 
@@ -453,6 +453,8 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
 
             //审核备注
             row.add(o.getAuditRemarks() == null ? "" : o.getAuditRemarks());
+
+            //zhuyuan1
 
             //服务商信息
             List<WorkOrderServerQuery> workOrderServerQueryList = workOrderServerService.queryByWorkOrderIdAndServerId(o.getId(), null);
