3ba8c6faf1b32e86c45577e6c78303f62c89de1c
zgw <1311510078@qq.com>
2022-04-18 18:05:29 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
@@ -2015,8 +2015,8 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
             return R.fail("审核通过的工单不允许修改");
         }
 
-        if (Objects.equals(oldWorkOrder.getStatus(),WorkOrder.STATUS_FINISHED)
-                && Objects.equals(oldWorkOrder.getAuditStatus(), workOrder.getAuditStatus())) {
+        if (!Objects.equals(oldWorkOrder.getStatus(),WorkOrder.STATUS_FINISHED)
+                && !Objects.equals(oldWorkOrder.getAuditStatus(), workOrder.getAuditStatus())) {
             return R.fail("不能审核未完结的工单");
         }
 
