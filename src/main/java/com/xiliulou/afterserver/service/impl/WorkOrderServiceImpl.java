e114c0c73f1c55e496fbd196aa65397c2b6d9d9f
zgw <1311510078@qq.com>
2022-04-21 16:23:50 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
@@ -2443,7 +2443,6 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
         }
 
         for(WorkOrderServerQuery item : workOrderServerList) {
-            log.info("专员提交数据 -- >" + JsonUtil.toJson(item));
             if (Objects.isNull(item.getServerId())) {
                 return R.fail("工单必须添加服务商");
             }
