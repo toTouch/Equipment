27deb8208dd0ea1008115c935b54c2c7d7521d14
zgw <1311510078@qq.com>
2022-06-13 15:17:01 +0800

    后台动态配置组件

--- src/main/java/com/xiliulou/afterserver/controller/admin/AdminJsonFileController.java
+++ src/main/java/com/xiliulou/afterserver/controller/admin/AdminJsonFileController.java
@@ -92,26 +92,26 @@ public class AdminJsonFileController {
             }
         }
 
-        if (Objects.equals(File.FILE_TYPE_PRODUCT_ACCESSORY_PACKAGING, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
-            List<File> fileList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_ACCESSORY_PACKAGING);
-            if (Objects.nonNull(fileList) && fileList.size() >= 4) {
-                return R.fail("附件包上传数量已达上限");
-            }
-        }
-
-        if (Objects.equals(File.FILE_TYPE_PRODUCT_OUTER_PACKAGING, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
-            List<File> fileList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_OUTER_PACKAGING);
-            if (Objects.nonNull(fileList) && fileList.size() >= 4) {
-                return R.fail("外包装上传数量已达上限");
-            }
-        }
-
-        if (Objects.equals(File.FILE_TYPE_PRODUCT_QUALITY_INSPECTION, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
-            List<File> fileSheepList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_QUALITY_INSPECTION);
-            if (Objects.nonNull(fileSheepList) && fileSheepList.size() == 1) {
-                fileService.removeFile(file.getId(), 0);
-            }
-        }
+//        if (Objects.equals(File.FILE_TYPE_PRODUCT_ACCESSORY_PACKAGING, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
+//            List<File> fileList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_ACCESSORY_PACKAGING);
+//            if (Objects.nonNull(fileList) && fileList.size() >= 4) {
+//                return R.fail("附件包上传数量已达上限");
+//            }
+//        }
+//
+//        if (Objects.equals(File.FILE_TYPE_PRODUCT_OUTER_PACKAGING, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
+//            List<File> fileList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_OUTER_PACKAGING);
+//            if (Objects.nonNull(fileList) && fileList.size() >= 4) {
+//                return R.fail("外包装上传数量已达上限");
+//            }
+//        }
+//
+//        if (Objects.equals(File.FILE_TYPE_PRODUCT_QUALITY_INSPECTION, file.getFileType()) && Objects.equals(File.TYPE_PRODUCT, file.getType())) {
+//            List<File> fileSheepList = fileService.queryByProductNewId(file.getBindId(), File.FILE_TYPE_PRODUCT_QUALITY_INSPECTION);
+//            if (Objects.nonNull(fileSheepList) && fileSheepList.size() == 1) {
+//                fileService.removeFile(file.getId(), 0);
+//            }
+//        }
 
         file.setCreateTime(System.currentTimeMillis());
         return R.ok(fileService.save(file));
