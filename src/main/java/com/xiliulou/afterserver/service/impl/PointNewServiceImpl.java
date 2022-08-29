26d5543ded0e7d66c2d53b877809284ea8a9f42f
zgw <1311510078@qq.com>
2022-08-29 17:44:51 +0800

    bug fix

--- src/main/java/com/xiliulou/afterserver/service/impl/PointNewServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/PointNewServiceImpl.java
@@ -203,12 +203,17 @@ public class PointNewServiceImpl extends ServiceImpl<PointNewMapper, PointNew> i
             return R.fail("数据库错误");
         }
 
+        log.info("点未绑定测试 1");
+
         if (pointNew.getProductIds().isEmpty()) {
             return R.fail("请传入点位id");
         }
 
+        log.info("点未绑定测试 2");
 
         pointNew.getProductIds().forEach(item -> {
+
+            log.info("点未绑定测试 3");
 
             PointProductBind oldPointProductBind = pointProductBindMapper
                     .selectOne(new QueryWrapper<PointProductBind>()
@@ -217,6 +222,8 @@ public class PointNewServiceImpl extends ServiceImpl<PointNewMapper, PointNew> i
             if(ObjectUtils.isNotNull(oldPointProductBind)){
                 pointProductBindMapper.deleteById(oldPointProductBind.getId());
             }
+
+            log.info("点未绑定测试 4");
 
             PointProductBind pointProductBind = new PointProductBind();
             pointProductBind.setPointId(pointNew.getId());
