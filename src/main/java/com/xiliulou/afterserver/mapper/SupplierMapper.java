e8ebab05d10bae7e1061553fc67c370bf04f685c
zgw <1311510078@qq.com>
2023-04-06 17:16:29 +0800

    fix(点位下拉):查询表更改

--- src/main/java/com/xiliulou/afterserver/mapper/SupplierMapper.java
+++ src/main/java/com/xiliulou/afterserver/mapper/SupplierMapper.java
@@ -7,10 +7,11 @@ import com.xiliulou.afterserver.web.vo.SupplierPullVo;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface SupplierMapper extends BaseMapper<Supplier> {
     @Select("select id, name from supplier where name like concat('%', #{name}, '%') limit 0, 20")
     List<SupplierPullVo> querySupplierPull(String name);
 
-    List<PageSearchVo> supplierSearch(Long offset, Long size, String name);
+    List<PageSearchVo> supplierSearch(@RequestParam("offset") Long offset, @RequestParam("size") Long size, @RequestParam("name") String name);
 }
