b11da224b30e8469bff39af63c014cd27be7edab
zgw <1311510078@qq.com>
2023-04-10 14:37:37 +0800

    fix(草莓优化):添加结算客户名称字段

--- src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/WorkOrderServiceImpl.java
@@ -344,7 +344,7 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
             "状态", "描述", "备注", "工单原因", "创建时间", "工单编号", "sn码", "审核内容", "专员", "派单时间"};
 
         String[] serverHeader = {"服务商", "工单费用", "结算方式", "解决方案", "解决时间", "处理时长", "文件个数","是否更换配件", "是否需要第三方承担费用", " 第三方类型",
-            "第三方公司", "应收第三方人工费", "应收第三方物料费", "支付状态", "第三方原因", "第三方对接人"};
+            "第三方公司", "应收第三方人工费","应收第三方运费", "应收第三方物料费", "支付状态", "第三方原因", "第三方对接人"};
 
         List<Product> productAll = productService.list();
 
@@ -694,6 +694,8 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
                         row.add(item.getThirdCompanyName() == null ? "" : item.getThirdCompanyName());
                         // "人工费用",
                         row.add(item.getArtificialFee() == null ? "" : item.getArtificialFee());
+                        //运费
+                        row.add(item.getDeliverFee() == null ? "" : item.getDeliverFee());
                         //物料费用
                         row.add(item.getMaterialFee() == null ? "" : item.getMaterialFee());
                         // "支付状态",
@@ -892,7 +894,7 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
             "状态", "描述", "备注", "工单原因", "创建时间", "工单编号", "sn码", "审核内容", "专员", "派单时间"};
 
         String[] serverHeader = {"服务商", "工单费用", "结算方式", "解决方案", "解决时间", "处理时长", "文件个数","是否更换配件", "是否需要第三方承担费用", " 第三方类型",
-            "第三方公司", "应收第三方人工费", "应收第三方物料费", "支付状态", "第三方原因", "第三方对接人"};
+            "第三方公司", "应收第三方人工费", "应收第三方运费","应收第三方物料费", "支付状态", "第三方原因", "第三方对接人"};
 
         List<Product> productAll = productService.list();
 
@@ -1199,6 +1201,8 @@ public class WorkOrderServiceImpl extends ServiceImpl<WorkOrderMapper, WorkOrder
                        row.add(item.getThirdCompanyName() == null ? "" : item.getThirdCompanyName());
                        // "人工费用",
                        row.add(item.getArtificialFee() == null ? "" : item.getArtificialFee());
+                       //运费
+                       row.add(item.getDeliverFee() == null ? "" : item.getDeliverFee());
                        //物料费用
                        row.add(item.getMaterialFee() == null ? "" : item.getMaterialFee());
                        // "支付状态",
