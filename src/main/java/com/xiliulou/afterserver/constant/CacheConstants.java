2cb05bc59c7f361fa0544f1e44afa92a2c4050fe
zgw <1311510078@qq.com>
2023-04-17 14:48:19 +0800

    fix(pda发货):资产编码重复问题修改

---
+++ src/main/java/com/xiliulou/afterserver/constant/CacheConstants.java
@@ -0,0 +1,11 @@
+package com.xiliulou.afterserver.constant;
+
+/**
+ * @author zgw
+ * @date 2023/4/17 14:41
+ * @mood
+ */
+public interface CacheConstants {
+    String CACHE_FACTORY_DELIVER_LOCK = "cache_factory_deliver_lock:";
+
+}
