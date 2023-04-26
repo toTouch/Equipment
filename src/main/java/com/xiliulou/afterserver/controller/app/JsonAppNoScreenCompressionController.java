e7f38acb646d5e426f8256df62ab8e67a2647aa3
zgw <1311510078@qq.com>
2023-04-26 18:45:00 +0800

    feat(压测回显):添加安卓压测回显

---
+++ src/main/java/com/xiliulou/afterserver/controller/app/JsonAppNoScreenCompressionController.java
@@ -0,0 +1,45 @@
+package com.xiliulou.afterserver.controller.app;
+
+import com.xiliulou.afterserver.service.ProductNewService;
+import com.xiliulou.afterserver.web.query.CabinetCompressionQuery;
+import com.xiliulou.afterserver.util.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zgw
+ * @date 2023/4/26 10:00
+ * @mood
+ */
+@RestController
+@RequestMapping("/app/noScreen/compression")
+public class JsonAppNoScreenCompressionController {
+
+    @Autowired
+    private ProductNewService productNewService;
+
+    /**
+     * 柜机压测状态上报
+     * @return
+     */
+
+    @PostMapping("upload")
+    public R cabinetCompressionStatus(CabinetCompressionQuery cabinetCompressionQuery){
+        return productNewService.cabinetCompressionStatus(cabinetCompressionQuery);
+    }
+
+    @GetMapping("check")
+    public R cabinetCompressionCheck(@RequestParam("sn")String sn){
+        return productNewService.cabinetCompressionCheck(sn);
+    }
+
+    @GetMapping("list")
+    public R cabinetCompressionList(@RequestParam("sn")String sn, @RequestParam("startTime") Long startTime, @RequestParam("endTime")Long endTime){
+        return productNewService.cabinetCompressionList(sn, startTime, endTime);
+    }
+}
---
+++ src/main/java/com/xiliulou/afterserver/web/vo/CabinetCompressionContentVo.java
@@ -0,0 +1,34 @@
+package com.xiliulou.afterserver.web.vo;
+
+import lombok.Data;
+
+/**
+ * @author zgw
+ * @date 2023/4/26 18:21
+ * @mood
+ */
+@Data
+public class CabinetCompressionContentVo {
+    /**
+     * 资产编码
+     */
+    private String sn;
+    /**
+     * 压测状态 0 失败 ，   1 成功   ， 2 压测中
+     */
+    private Integer testStatus;
+    /**
+     * 压测开始时间（压测结束无需上报）
+     */
+    private Long testStartTime;
+    /**
+     * 压测结束时间（压测开始无需上报）
+     */
+    private Long testEndTime;
+    /**
+     * 压测错误原因
+     */
+    private String testMsg;
+
+    private String content;
+}
