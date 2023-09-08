0466fa4547c188fde11dcca5ab144b653889c0c2
lnyyy <1053452188@qq.com>
2023-09-08 17:31:32 +0800

    资产编码为成功或者失败的时候 不修改为测试中

--- src/main/java/com/xiliulou/afterserver/service/impl/ProductNewServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/ProductNewServiceImpl.java
@@ -1511,14 +1511,12 @@ public class ProductNewServiceImpl extends ServiceImpl<ProductNewMapper, Product
         if (Objects.isNull(productNew)) {
             return R.fail(null, "未查询到相关资产编码");
         }
-        // 判断当前资产编码是不是 成功或者失败 若是 直接返回
-        if (Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_FAIL) || Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_SUCC)){
-            return R.ok();
-        }
-
         ProductNew productUpdate = new ProductNew();
         productUpdate.setId(productNew.getId());
-        productUpdate.setTestResult(cabinetCompressionQuery.getTestStatus());
+        // 判断当前资产编码是不是 成功或者失败 若是 不修改状态
+        if (!Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_FAIL) && !Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_SUCC)){
+            productUpdate.setTestResult(cabinetCompressionQuery.getTestStatus());
+        }
         productUpdate.setTestStartTime(cabinetCompressionQuery.getTestStartTime());
         productUpdate.setTestEndTime(cabinetCompressionQuery.getTestEndTime());
         productUpdate.setTestMsg(cabinetCompressionQuery.getTestMsg());
@@ -1562,7 +1560,10 @@ public class ProductNewServiceImpl extends ServiceImpl<ProductNewMapper, Product
         }
         ProductNew productUpdate = new ProductNew();
         productUpdate.setId(productNew.getId());
-        productUpdate.setTestResult(cabinetCompressionQuery.getTestStatus());
+        // 判断当前资产编码是不是 成功或者失败 若是 不修改状态
+        if (!Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_FAIL) && !Objects.equals(productNew.getTestResult(), CompressionQuery.TEST_SUCC)){
+            productUpdate.setTestResult(cabinetCompressionQuery.getTestStatus());
+        }
         productUpdate.setTestStartTime(cabinetCompressionQuery.getTestStartTime());
         productUpdate.setTestEndTime(cabinetCompressionQuery.getTestEndTime());
         productUpdate.setTestMsg(cabinetCompressionQuery.getTestMsg());
