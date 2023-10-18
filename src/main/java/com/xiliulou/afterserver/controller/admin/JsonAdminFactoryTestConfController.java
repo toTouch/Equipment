337be19d84c9fea47f1c918b0f80dcfcb7a33f82
Touch <zbz1206@outlook.com>
2023-10-18 15:04:19 +0800

    feature(AdminJsonProductNewController.java FactoryTestConf.java FactoryTestConfMapper.java FactoryTestConfMapper.xml FactoryTestConfService.java FactoryTestConfServiceImpl.java JsonAdminFactoryTestConfController.java): 增加配置流程上架、下架功能
    增加配置流程上架、下架功能，可以上架多个；给app传上架的配置流程(/after-service/app/compression/listConf)

--- src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminFactoryTestConfController.java
+++ src/main/java/com/xiliulou/afterserver/controller/admin/JsonAdminFactoryTestConfController.java
@@ -1,15 +1,20 @@
 package com.xiliulou.afterserver.controller.admin;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xiliulou.afterserver.entity.FactoryTestConf;
-import com.xiliulou.afterserver.service.AuditGroupService;
 import com.xiliulou.afterserver.service.FactoryTestConfService;
 import com.xiliulou.afterserver.util.R;
-import com.xiliulou.afterserver.web.query.AuditGroupStrawberryQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zgw
@@ -20,33 +25,43 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequestMapping("/admin/factoryTestConf")
 public class JsonAdminFactoryTestConfController {
+    
     @Autowired
     FactoryTestConfService factoryTestConfService;
-
+    
     @GetMapping("/list")
-    public R queryList(@RequestParam("offset")Long offset, @RequestParam("size")Long size,@RequestParam(required = false,value = "confName") String confName){
-        return factoryTestConfService.getPage(offset,size,confName);
+    public R queryList(@RequestParam("offset") Long offset, @RequestParam("size") Long size, @RequestParam(required = false, value = "confName") String confName) {
+        return factoryTestConfService.getPage(offset, size, confName);
     }
-
+    
     @PostMapping("/save")
-    public R saveOne(@RequestBody @Validated FactoryTestConf con){
+    public R saveOne(@RequestBody @Validated FactoryTestConf con) {
         return factoryTestConfService.saveOne(con);
     }
-
-
+    
+    
     @DeleteMapping("/del/{id}")
-    public R reomveOne(@PathVariable("id")Long id){
+    public R reomveOne(@PathVariable("id") Long id) {
         return factoryTestConfService.reomveOne(id);
     }
+    
     @GetMapping("/detail/{id}")
-    public R detail(@PathVariable("id")Long id){
+    public R detail(@PathVariable("id") Long id) {
         return factoryTestConfService.detailById(id);
     }
-
+    
+    /**
+     * 上下架工厂测试流程配置
+     */
+    @PutMapping("/shelf/{id}")
+    public R shelfOne(@PathVariable("id") Long id, @RequestParam("status") Integer status) {
+        return factoryTestConfService.updateOneShelf(id, status);
+    }
+    
     @PutMapping("/update")
-    public R updateOne(@RequestBody @Validated FactoryTestConf con){
+    public R updateOne(@RequestBody @Validated FactoryTestConf con) {
         return factoryTestConfService.updateOne(con);
     }
-
-
+    
+    
 }
