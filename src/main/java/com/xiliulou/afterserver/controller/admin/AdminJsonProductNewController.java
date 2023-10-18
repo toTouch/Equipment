7f4606fa1d4f4ab4ff6041bb7d6b0d3dced98acd
Touch <zbz1206@outlook.com>
2023-10-18 15:04:19 +0800

    feature(AdminJsonProductNewController.java FactoryTestConf.java FactoryTestConfMapper.java FactoryTestConfMapper.xml FactoryTestConfService.java FactoryTestConfServiceImpl.java JsonAdminFactoryTestConfController.java): 增加配置流程上架、下架功能
    增加配置流程上架、下架功能，可以上架多个；给app传上架的配置流程(/after-service/app/compression/listConf)

--- src/main/java/com/xiliulou/afterserver/controller/admin/AdminJsonProductNewController.java
+++ src/main/java/com/xiliulou/afterserver/controller/admin/AdminJsonProductNewController.java
@@ -1,22 +1,27 @@
 package com.xiliulou.afterserver.controller.admin;
 
-import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
-import com.xiliulou.afterserver.entity.*;
-import com.xiliulou.afterserver.service.*;
+import com.xiliulou.afterserver.entity.File;
+import com.xiliulou.afterserver.service.ProductNewService;
 import com.xiliulou.afterserver.util.R;
-import com.xiliulou.afterserver.util.SecurityUtils;
-import com.xiliulou.afterserver.web.query.CompressionQuery;
-import com.xiliulou.afterserver.web.query.ProductNewDetailsQuery;
 import com.xiliulou.afterserver.web.query.ProductNewQuery;
-import com.xiliulou.afterserver.web.vo.OssUrlVo;
 import com.xiliulou.storage.config.StorageConfig;
 import com.xiliulou.storage.service.impl.AliyunOssService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Hardy
@@ -26,113 +31,111 @@ import java.util.*;
 @RestController
 @Slf4j
 public class AdminJsonProductNewController {
+    
     @Autowired
     private ProductNewService productNewService;
+    
     //    @Autowired
-//    private ProductService productService;
-//    @Autowired
-//    private BatchService batchService;
-//    @Autowired
-//    private PointProductBindService pointProductBindService;
-//    @Autowired
-//    private PointNewService pointNewService;
-//    @Autowired
-//    private SupplierService supplierService;
-//    @Autowired
-//    private WarehouseService warehouseService;
-//    @Autowired
-//    private IotCardService iotCardService;
-//    @Autowired
-//    private CameraService cameraService;
+    //    private ProductService productService;
+    //    @Autowired
+    //    private BatchService batchService;
+    //    @Autowired
+    //    private PointProductBindService pointProductBindService;
+    //    @Autowired
+    //    private PointNewService pointNewService;
+    //    @Autowired
+    //    private SupplierService supplierService;
+    //    @Autowired
+    //    private WarehouseService warehouseService;
+    //    @Autowired
+    //    private IotCardService iotCardService;
+    //    @Autowired
+    //    private CameraService cameraService;
     @Autowired
     private AliyunOssService aliyunOssService;
+    
     @Autowired
     private StorageConfig StorageConfig;
-//    @Autowired
-//    private ColorCardService colorCardService;
-
+    //    @Autowired
+    //    private ColorCardService colorCardService;
+    
     //@PostMapping("/admin/productNew")
-//    public R saveAdminPointNew(@RequestBody ProductNew productNew){
-//        return productNewService.saveAdminProductNew(productNew);
-//    }
-
+    //    public R saveAdminPointNew(@RequestBody ProductNew productNew){
+    //        return productNewService.saveAdminProductNew(productNew);
+    //    }
+    
     @PutMapping("/admin/productNew")
     public R putAdminPointNew(@RequestBody ProductNewQuery query) {
         return productNewService.putAdminProductNew(query);
     }
-
-   /*
-    * 删除产品
-    */
+    
+    /*
+     * 删除产品
+     */
     @DeleteMapping("/admin/productNew/{id}")
     public R delAdminPointNew(@PathVariable("id") Long id) {
         return productNewService.delAdminProductNew(id);
     }
-
+    
     /**
      * 产品列表
-     * @param offset 查询起始位置
-     * @param limit 条数
-     * @param no 产品编号
-     * @param modelId 产品型号
-     * @param pointId 产品位置
+     *
+     * @param offset    查询起始位置
+     * @param limit     条数
+     * @param no        产品编号
+     * @param modelId   产品型号
+     * @param pointId   产品位置
      * @param pointType 产品位置类型
      * @param startTime 开始时间
-     * @param endTime  结束时间
-     * @param testType 测试类型
+     * @param endTime   结束时间
+     * @param testType  测试类型
      * @return
      */
-
+    
     @GetMapping("/admin/productNew/list")
-    public R pointList(@RequestParam("offset") Integer offset,
-                       @RequestParam("limit") Integer limit,
-                       @RequestParam(value = "no", required = false) String no,
-                       @RequestParam(value = "modelId", required = false) Long modelId,
-                       @RequestParam(value = "pointId", required = false) Long pointId,
-                       @RequestParam(value = "pointType", required = false) Integer pointType,
-                       @RequestParam(value = "startTime", required = false) Long startTime,
-                       @RequestParam(value = "endTime", required = false) Long endTime,
-                       @RequestParam(value = "testType", required = false) String testType,
-                       @RequestParam(value = "cabinetSn", required = false) String cabinetSn) {
-        return productNewService.pointList(offset, limit, no, modelId, pointId, pointType, startTime, endTime, testType,cabinetSn);
-
+    public R pointList(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit, @RequestParam(value = "no", required = false) String no,
+            @RequestParam(value = "modelId", required = false) Long modelId, @RequestParam(value = "pointId", required = false) Long pointId,
+            @RequestParam(value = "pointType", required = false) Integer pointType, @RequestParam(value = "startTime", required = false) Long startTime,
+            @RequestParam(value = "endTime", required = false) Long endTime, @RequestParam(value = "testType", required = false) String testType,
+            @RequestParam(value = "cabinetSn", required = false) String cabinetSn) {
+        return productNewService.pointList(offset, limit, no, modelId, pointId, pointType, startTime, endTime, testType, cabinetSn);
+        
     }
-
-
+    
+    
     //查看图片
     @GetMapping("/admin/productNew/{id}")
     public R getProductFile(@PathVariable("id") Long id) {
         return productNewService.getProductFile(id, File.FILE_TYPE_PRODUCT_PRODUCT);
     }
-
+    
     //更新
     @PutMapping("/admin/productNew/update/status")
-    public R updateStatusFromBatch(@RequestParam(value = "ids", required = false) List<Long> ids,
-                                   @RequestParam("status") Integer status) {
+    public R updateStatusFromBatch(@RequestParam(value = "ids", required = false) List<Long> ids, @RequestParam("status") Integer status) {
         if (ids.isEmpty()) {
             return R.fail("id不能为空");
         }
-
+        
         return productNewService.updateStatusFromBatch(ids, status);
-
+        
     }
-
+    
     @GetMapping("/admin/queryProductInfo")
     public R queryProductInfo(String no) {
         return productNewService.queryProductInfo(no);
     }
-
+    
     @GetMapping("/admin/queryLikeProductByNo")
     public R queryLikeProductByNo(String no) {
         return productNewService.queryLikeProductByNo(no);
     }
-
+    
     @PostMapping("/admin/bindPoint")
     public R bindPoint(Long productId, Long pointId, Integer pointType) {
         return productNewService.bindPoint(productId, pointId, pointType);
     }
-
-
+    
+    
     /**
      * 根据资产编码拉取物联网卡信息
      *
@@ -143,8 +146,8 @@ public class AdminJsonProductNewController {
     public R findIotCard(@RequestParam("no") String no) {
         return productNewService.findIotCard(no);
     }
-
-
+    
+    
     /**
      * 下载柜机测试文件
      *
@@ -157,7 +160,7 @@ public class AdminJsonProductNewController {
         String testFileName = "";
         final String HTTP = "http";
         final String HTTPS = "https";
-
+        
         if (StringUtils.isNotBlank(StorageConfig.getTestFileDir())) {
             testFileName = StorageConfig.getTestFileDir();
         }
@@ -166,13 +169,13 @@ public class AdminJsonProductNewController {
         } catch (Exception e) {
             log.error("oss error!", e);
         }
-
+        
         if (Objects.isNull(url)) {
             return R.fail("oss error");
         }
-
+        
         url = HTTPS + url.substring(4);
-
+        
         Map<String, String> result = new HashMap<>(1);
         result.put("url", url);
         return R.ok(result);
--- src/main/java/com/xiliulou/afterserver/service/impl/FactoryTestConfServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/FactoryTestConfServiceImpl.java
@@ -3,7 +3,6 @@ package com.xiliulou.afterserver.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -30,10 +29,10 @@ import java.util.Objects;
 @Slf4j
 @Service
 public class FactoryTestConfServiceImpl extends ServiceImpl<FactoryTestConfMapper, FactoryTestConf> implements FactoryTestConfService {
-
+    
     @Resource
     private FactoryTestConfMapper factoryTestConfMapper;
-
+    
     @Override
     public List<FactoryTestConf> getAllByApi(ApiRequestQuery apiRequestQuery) {
         FactoryTestConf condition = new FactoryTestConf();
@@ -43,82 +42,97 @@ public class FactoryTestConfServiceImpl extends ServiceImpl<FactoryTestConfMappe
         }
         return factoryTestConfMapper.queryAll(condition);
     }
-
-
+    
+    
     @Override
     public R getPage(Long offset, Long size, String confName) {
         Page page = PageUtil.getPage(offset, size);
         FactoryTestConf conf = new FactoryTestConf();
         conf.setConfName(confName);
         LambdaQueryWrapper<FactoryTestConf> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(Objects.nonNull(conf.getId()), FactoryTestConf::getId, conf.getId())
-                .like(Objects.nonNull(conf.getConfName()), FactoryTestConf::getConfName, conf.getConfName())
-                .eq(FactoryTestConf::getDelFlag,FactoryTestConf.DEL_NORMAL)
-                .orderByDesc(FactoryTestConf::getCreateTime);
+        wrapper.eq(Objects.nonNull(conf.getId()), FactoryTestConf::getId, conf.getId()).like(Objects.nonNull(conf.getConfName()), FactoryTestConf::getConfName, conf.getConfName())
+                .eq(FactoryTestConf::getDelFlag, FactoryTestConf.DEL_NORMAL).orderByDesc(FactoryTestConf::getCreateTime);
         return R.ok(factoryTestConfMapper.selectPage(page, wrapper));
     }
-
+    
     @Override
     public R reomveOne(Long id) {
-        if(Objects.isNull(id)){
-            return  R.fail("id不能为空");
+        if (Objects.isNull(id)) {
+            return R.fail("id不能为空");
         }
         FactoryTestConf updater = new FactoryTestConf();
         updater.setDelFlag(FactoryTestConf.DEL_DEL);
         updater.setId(id);
         LambdaUpdateWrapper<FactoryTestConf> updateWrapper = Wrappers.lambdaUpdate();
-
+        
         updateWrapper.eq(Objects.nonNull(id), FactoryTestConf::getId, updater.getId());
-
+        
         factoryTestConfMapper.update(updater, updateWrapper);
         return R.ok();
     }
-
+    
     @Override
-    public R saveOne(FactoryTestConf conf){
-        if(Objects.isNull(conf.getConfName())){
-            return  R.fail("配置名称不能为空");
+    public R saveOne(FactoryTestConf conf) {
+        if (Objects.isNull(conf.getConfName())) {
+            return R.fail("配置名称不能为空");
         }
-        if(Objects.isNull(conf.getJsonContent())){
-            return  R.fail("配置内容不能为空");
+        if (Objects.isNull(conf.getJsonContent())) {
+            return R.fail("配置内容不能为空");
         }
         conf.setCreateTime(System.currentTimeMillis());
         conf.setUpdateTime(System.currentTimeMillis());
         int row = baseMapper.insert(conf);
-        if (row!=0){
+        if (row != 0) {
             return R.ok();
         }
         return R.fail("数据异常");
     }
-
+    
     @Override
-    public R updateOne(FactoryTestConf updater){
-
-        if(Objects.isNull(updater.getId())){
-            return  R.fail("id不能为空");
+    public R updateOne(FactoryTestConf updater) {
+        
+        if (Objects.isNull(updater.getId())) {
+            return R.fail("id不能为空");
         }
-        if(Objects.isNull(updater.getConfName())){
-            return  R.fail("配置名称不能为空");
+        if (Objects.isNull(updater.getConfName())) {
+            return R.fail("配置名称不能为空");
         }
-        if(Objects.isNull(updater.getJsonContent())){
-            return  R.fail("配置内容不能为空");
+        if (Objects.isNull(updater.getJsonContent())) {
+            return R.fail("配置内容不能为空");
         }
         updater.setUpdateTime(System.currentTimeMillis());
         boolean result = updateById(updater);
-        if (result){
+        if (result) {
             return R.ok();
         }
         return R.fail("更新失败");
     }
-
+    
     @Override
-    public R detailById(Long id){
-        if(Objects.isNull(id)){
-            return  R.fail("id不能为空");
+    public R updateOneShelf(Long id, Integer shelfStatus) {
+        if (Objects.isNull(id)) {
+            return R.fail("id不能为空");
+        }
+        
+        FactoryTestConf updater = new FactoryTestConf();
+        updater.setUpdateTime(System.currentTimeMillis());
+        updater.setId(id);
+        updater.setShelfStatus(shelfStatus);
+        int result = factoryTestConfMapper.updateOneShelf(updater);
+        if (result > 0) {
+            return R.ok();
+        }
+        return R.fail("上下架失败");
+    }
+    
+    @Override
+    public R detailById(Long id) {
+        if (Objects.isNull(id)) {
+            return R.fail("id不能为空");
         }
         FactoryTestConf conf = getById(id);
-        if (Objects.isNull(conf)){
-            return  R.fail("记录不存在");
+        if (Objects.isNull(conf)) {
+            return R.fail("记录不存在");
         }
         return R.ok(conf);
     }
