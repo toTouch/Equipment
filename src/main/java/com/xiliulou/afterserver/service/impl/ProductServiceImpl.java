e00f918b05424945b2a4c58ea9d002aa074c70ea
zhangbozhi <zbz1206@outlook.com>
2024-08-05 13:56:39 +0800

    refactor: 去除 records 层

--- src/main/java/com/xiliulou/afterserver/service/impl/ProductServiceImpl.java
+++ src/main/java/com/xiliulou/afterserver/service/impl/ProductServiceImpl.java
@@ -270,9 +270,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
     @Override
     public R getSerialNumberPage(Long offset, Long size, ProductSerialNumberQuery productSerialNumber) {
         List<ProductNew> data = productNewMapper.selectNoPull(productSerialNumber.getNo(),offset,size);
-        Map records = new HashMap();
-        records.put("records", data);
-        return R.ok(records);
+        return R.ok(data);
     }
     
     @Override
